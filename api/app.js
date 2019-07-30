0cbd7b7cf394718624f142cb4a2dbeefe542c8e6
GusAntoniassi <gus.antoniassi@gmail.com>
2019-07-30 13:42:15 -0300

    Ajustar cabeçalhos CORS para permitir método OPTIONS e cabeçalho x-access-token

--- api/app.js
+++ api/app.js
@@ -22,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE,OPTIONS");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token');
   next();
 })
 
