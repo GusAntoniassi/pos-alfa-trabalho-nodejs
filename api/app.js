745dffe7a7251a1a261db27bec8c03d52b854219
GusAntoniassi <gus.antoniassi@gmail.com>
2019-08-01 07:17:21 -0300

    Implementar correções de segurança npm

--- api/app.js
+++ api/app.js
@@ -11,8 +11,9 @@ var tarefasRouter = require('./routes/tarefas');
 var app = express();
 
 // view engine setup
+app.engine('pug', require('pug').__express)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
